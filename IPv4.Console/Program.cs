5e3e01e613ae94d97247c768f6e7310f58c8b092
Jonas Ababio <jonasagesmilly@outlook.com>
2022-02-22 19:25:08 +0000

    Add Selection prompt tp Program.cs

--- IPv4.Console/Program.cs
+++ IPv4.Console/Program.cs
@@ -2,3 +2,7 @@
 using Spectre.Console;
 
 AnsiConsole.Write(new FigletText("IPv4 Addressing").Color(Color.Red));
+
+var choice = AnsiConsole.Prompt(new SelectionPrompt<string>().PageSize(10)
+    .Title("[lightgreen]?[/] What do you [green]want to do[/]?")
+    .AddChoices("IP Basics", "IP Subnetting"));
