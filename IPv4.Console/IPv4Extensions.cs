144091e61a749317cca5335db0f4d60426394fc0
Jonas Ababio <jonasagesmilly@outlook.com>
2022-02-24 20:45:22 +0000

    Add GenerateMask method to IPv4Extensions

--- IPv4.Console/IPv4Extensions.cs
+++ IPv4.Console/IPv4Extensions.cs
@@ -36,5 +36,38 @@ namespace IPv4.Console
         {
             return IPAddress.TryParse(ip, out _);
         }
+
+        public static string GenerateMask(int networkBits)
+        {
+            int hostBits = 32 - networkBits;
+
+            StringBuilder builder = new(32);
+
+            for (int i = 0; i < networkBits; i++)
+            {
+                builder.Append('1');
+            }
+
+            for (int i = 0; i < hostBits; i++)
+            {
+                builder.Append('0');
+            }
+
+            string mask = builder.ToString();
+
+            var octects = mask.Chunk(8).ToList();
+
+            string firstOctect = Convert.ToString(Convert.ToInt32(new string(octects[0]), 2), 10);
+            string secondOctect = Convert.ToString(Convert.ToInt32(new string(octects[1]), 2), 10);
+            string thirdOctect = Convert.ToString(Convert.ToInt32(new string(octects[2]), 2), 10);
+            string fourthOctect = Convert.ToString(Convert.ToInt32(new string(octects[3]), 2), 10);
+
+            string[] decOctects = new[] {firstOctect, secondOctect, thirdOctect, fourthOctect};
+
+            StringBuilder builder1 = new();
+            builder1.AppendJoin('.', decOctects);
+
+            return builder1.ToString();
+        }
     }
 }
