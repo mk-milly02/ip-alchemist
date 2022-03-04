3633686a2465a3d18b717b1d4267dc864b59ebb7
Jonas Ababio <jonasagesmilly@outlook.com>
2022-03-04 14:58:12 +0000

    Prompt user for available address and mask

--- IPv4.Console/BasicAddressing.cs
+++ IPv4.Console/BasicAddressing.cs
@@ -1,9 +1,32 @@
-using Spectre.Console.Cli;
+using Spectre.Console;
 
-public class BasicAddressing
+namespace IPv4.Console
 {
-    public static void Run()
+    public static class BasicAddressing
     {
-        var app = new CommandApp();
+        private static BasicNetwork? Network { get; set; }
+
+        static string AskForAvailableIPAddress()
+        {
+            var ip = AnsiConsole.Prompt(
+                new TextPrompt<string>("[lime]?[/] What is the available IP Address [blue]eg. 196.128.0.4[/]: ")
+                .Validate(ip => IPv4Extensions.IsIPAddress(ip)
+                ? ValidationResult.Success()
+                : ValidationResult.Error("[red]! This is not a vaild IP address.[/]")));
+
+            return ip;
+        }
+
+        static int AskForNetworkMask()
+        {
+            var mask = AnsiConsole.Prompt(
+                new TextPrompt<string>("[lime]?[/] Enter the network mask: ")
+                .Validate(mask => IPv4Extensions.ValidateNetworkMask(mask)
+                ? ValidationResult.Success()
+                : ValidationResult.Error("[red]! A prefix length must be between 1 and 32")));
+
+            return int.Parse(mask);
+        }
+
     }
 }
