426bdef5080913a11aa9661560a3f3d7f82d1e76
Jonas Ababio <jonasagesmilly@outlook.com>
2022-03-04 15:14:52 +0000

    Disable nullable

--- IPv4.Console/BasicNetwork.cs
+++ IPv4.Console/BasicNetwork.cs
@@ -4,17 +4,17 @@ namespace IPv4.Console
 {
     public class BasicNetwork
     {
-        public IPAddress? AvailableAddress { get; set; }
+        public IPAddress AvailableAddress { get; set; }
 
-        public IPAddress? NetworkMask { get; set; }
+        public IPAddress NetworkMask { get; set; }
 
         public int NetworkBits { get; set; }
 
-        public IPAddress? NetworkAddress { get; set; }
+        public IPAddress NetworkAddress { get; set; }
 
-        public IPAddress? BroadcastAddress { get; set; }
+        public IPAddress BroadcastAddress { get; set; }
 
-        public string? Range { get; set; }
+        public string Range { get; set; }
 
         public int TotalHosts { get; set; }
 
