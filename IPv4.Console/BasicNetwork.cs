0010f9c98b68de5c89b29d162a3afa58d64fa5d4
Jonas Ababio <jonasagesmilly@outlook.com>
2022-03-05 21:09:35 +0000

    Add Tabulate method

--- IPv4.Console/BasicNetwork.cs
+++ IPv4.Console/BasicNetwork.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using Spectre.Console;
 
 namespace IPv4.Console
 {
@@ -6,18 +7,36 @@ namespace IPv4.Console
     {
         public IPAddress AvailableAddress { get; set; }
 
-        public IPAddress NetworkMask { get; set; }
+        public IPAddress NetworkMask => IPv4Extensions.GenerateNetworkMask(NetworkBits);
 
         public int NetworkBits { get; set; }
 
-        public IPAddress NetworkAddress { get; set; }
+        public IPAddress NetworkAddress => IPv4Extensions.GetNetworkAddress(AvailableAddress, NetworkMask);
 
-        public IPAddress BroadcastAddress { get; set; }
+        public IPAddress BroadcastAddress => IPv4Extensions.GetBroadcastAddress(NetworkAddress, NetworkMask);
 
-        public string Range { get; set; }
+        public string Range => IPv4Extensions.GetRange(NetworkAddress, BroadcastAddress);
 
-        public int TotalHosts { get; set; }
+        public int TotalHosts => IPv4Extensions.TotalNumberOfAddresses(NetworkBits);
 
-        public int TotalValidHosts { get; set; }
+        public int TotalValidHosts => TotalHosts - 2;
+
+        public void Tabulate()
+        {
+            AnsiConsole.Write("\n");
+
+            Table output = new();
+            output.Border(TableBorder.Rounded);
+            output.BorderColor(Color.Gold1);
+            output.AddColumns("", "");
+            output.AddRow("Network mask", NetworkMask.ToString());
+            output.AddRow("[red]Network Address[/]", NetworkAddress.ToString());
+            output.AddRow("Broadcast Address", BroadcastAddress.ToString());
+            output.AddRow("[lime]Addressess(Total)[/]", TotalHosts.ToString());
+            output.AddRow("Valid Host", TotalValidHosts.ToString());
+            output.AddRow("Range", $"[yellow]{Range}[/]");
+
+            AnsiConsole.Write(output);
+        }
     }
 }
