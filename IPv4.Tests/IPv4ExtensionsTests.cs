da14875d7df8b3fb1c47175506c9466f21a10fbf
Jonas Ababio <jonasagesmilly@outlook.com>
2022-03-05 20:06:29 +0000

    Write test for GenerateNetworkMask method

--- IPv4.Tests/IPv4ExtensionsTests.cs
+++ IPv4.Tests/IPv4ExtensionsTests.cs
@@ -6,99 +6,13 @@ namespace IPv4.Tests
 {
     public class IPv4ExtensionsTests
     {
-        [Theory]
-        [InlineData("196.65.45.4/2")]
-        [InlineData("196.65.45.4/8")]
-        [InlineData("196.65.45.4/16")]
-        [InlineData("196.65.45.4/24")]
-        [InlineData("196.65.45.4/4")]
-        public void ValidateMask(string ip)
-        {
-            Assert.True(IPv4Extensions.ValidateMask(ip));
-        }
-
         [Fact]
-        public void ReturnTrue_IsIPValid()
+        public void ShouldPass_GenerateNetworkMask()
         {
             // Given
-            string ip = "196.65.45.4";
-
-            Assert.True(IPv4Extensions.IsIPValid(ip));
-        }
-
-        [Theory]
-        [InlineData("196.65.45.4")]
-        [InlineData("196.0.45.4")]
-        [InlineData("3.65.45")]
-        [InlineData("0")]
-        [InlineData("196.65")]
-        public void IsIPValid(string ip)
-        {
-            Assert.True(IPv4Extensions.IsIPValid(ip));
-        }
-
-        [Fact]
-        public void ShouldPass_GenerateMask()
-        {
-            // Given
-            string expected = "255.255.255.0";
-            // Then
-            string actual = IPv4Extensions.GenerateMask(24);
-
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void ShouldPass_GetBroadcastAddress()
-        {
-            // Given
-            string ip = "196.168.0.1"; int netBits = 16;
-
-            string mask = IPv4Extensions.GenerateMask(netBits);
-            string netAd = IPv4Extensions.GetNetworkAddress(ip, mask);
-            string broadAd = IPv4Extensions.GetBroadcastAddress(netAd, mask);
-            // Then
-            Assert.True(netAd.Equals("196.168.0.0"));
-            Assert.True(broadAd.Equals("196.168.255.255"));
-        }
-
-        [Fact]
-        public void ShouldPass_TotalNumberOfAddresses()
-        {
-            // Given
-            int expected = 256;
+            string expected = "255.255.255.192";
             // When
-            int actual = IPv4Extensions.TotalNumberOfAddresses(24);
-            // Then
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void ReturnTrue()
-        {
-            bool output = IPv4Extensions.ValidateHostGroups("10");
-
-            Assert.True(output);
-        }
-
-        [Fact]
-        public void ShouldPass_GetHostGroups()
-        {
-            // Given
-            string hosts = "10, 20, 30, 40";
-            // When
-            List<int> actual = IPv4Extensions.GetHostGroups(hosts);
-            // Then
-            Assert.True(actual.Count.Equals(4));
-        }
-
-        [Fact]
-        public void ReturnTrue_FindPowerOfTwo()
-        {
-            // Given
-            int expected = 128;
-            // When
-            int actual = IPv4Extensions.FindPowerOfTwo(125);
+            string actual = IPv4Extensions.GenerateNetworkMask(26).ToString();
             // Then
             Assert.Equal(expected, actual);
         }
