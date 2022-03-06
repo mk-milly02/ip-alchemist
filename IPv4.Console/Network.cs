ccfd23cd8cfd692429e89ad01ff702e76e588434
Jonas Ababio <jonasagesmilly@outlook.com>
2022-03-06 21:33:39 +0000

    Add networkBits to output table

--- IPv4.Console/Network.cs
+++ IPv4.Console/Network.cs
@@ -31,7 +31,8 @@ namespace IPv4.Console
             output.MinimalBorder();
             output.Width(70);
             output.AddColumns($"{Emoji.Known.Rocket}", "[violet]Network Credentials[/]");
-            output.AddRow("Network mask", NetworkMask.ToString());
+            output.AddRow("[cyan]Network mask[/]", NetworkMask.ToString());
+            output.AddRow("Network Bits", NetworkBits.ToString());
             output.AddRow("[red]Network Address[/]", NetworkAddress.ToString());
             output.AddRow("Broadcast Address", BroadcastAddress.ToString());
             output.AddRow("[lime]Addressess(Total)[/]", TotalHosts.ToString());
