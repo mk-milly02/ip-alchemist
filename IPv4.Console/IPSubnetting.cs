ec056832a3ea09aad0c499a5a0c82392e37e5008
Jonas Ababio <jonasagesmilly@outlook.com>
2022-03-07 19:37:20 +0000

    Implement new validator for NumberOfSubnets prompt

--- IPv4.Console/IPSubnetting.cs
+++ IPv4.Console/IPSubnetting.cs
@@ -21,12 +21,32 @@ namespace IPv4.Console
             var subnets = AnsiConsole.Prompt(
                 new TextPrompt<string>("[lime]?[/] Enter your desired number of subnets: ")
                 .PromptStyle(new Style(Color.DarkOrange))
-                .Validate(number => int.TryParse(number, out _)
+                .Validate(number => IPv4Extensions.ValidateNumberOfSubnets(number)
                 ? ValidationResult.Success()
                 : ValidationResult.Error("[red]! Invalid number of subnets.[/]"))
             );
 
             return int.Parse(subnets);
+        }
+
+        static List<int> AskForNumberOfHosts()
+        {
+            int numberOfSubnets = AskForNumberOfSubnets();
+            List<int> output = new();
+
+            for (int i = 0; i < numberOfSubnets; i++)
+            {
+                var hosts = AnsiConsole.Prompt(
+                    new TextPrompt<string>($"[lime]?[/] Enter number of hosts in subnet {i++}: ")
+                    .PromptStyle(new Style(Color.Purple4))
+                    .Validate(number => int.TryParse(number, out _)
+                    ? ValidationResult.Success()
+                    : ValidationResult.Error("[red]! Invalid number of hosts.[/]")));
+
+                output.Add(int.Parse(hosts));
+            }
+
+            return output;
         }
     }
 }
