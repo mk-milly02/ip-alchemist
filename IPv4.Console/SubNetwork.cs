8b88be96df70be6b39b72c722888b2ba1578c6e2
Jonas Ababio <jonasagesmilly@outlook.com>
2022-03-09 20:53:43 +0000

    Override Tabulate method

--- IPv4.Console/SubNetwork.cs
+++ IPv4.Console/SubNetwork.cs
@@ -1,9 +1,18 @@
+using System.Net;
+
 namespace IPv4.Console
 {
     public class SubNetwork : Network
     {
         public int DesiredHost { get; set; } 
 
-        public int SpareHostAddresses  => TotalHosts - DesiredHost; 
+        public int SpareHostAddresses  => TotalHosts - DesiredHost;
+
+        public IPAddress NextAvailableIP { get; set; }
+
+        public override void Tabulate()
+        {
+            
+        }
     }
 }
