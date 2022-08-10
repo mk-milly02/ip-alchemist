82024933d947b14314249659b982ce05d4f655d9
Jonas Ababio <jonasagesmilly@outlook.com>
2022-08-10 10:39:15 +0000

    Change IP Subnetting choice to choice group

--- IPv4.Console/Program.cs
+++ IPv4.Console/Program.cs
@@ -2,13 +2,18 @@
 using IPv4.Console;
 using Spectre.Console;
 
-AnsiConsole.Write(new FigletText("IPv4 Addressing").Color(Color.Orange3));
-AnsiConsole.MarkupLine("[dim italic]Made by Jonas[/]");
+Console.Title = "IPv4 Calculator";
+
+AnsiConsole.Write(new FigletText("IPv4 Calculator").Color(Color.Orange3));
+
+AnsiConsole.MarkupLine("[dim italic]Made by mk-milly02[/]");
+
 AnsiConsole.WriteLine("\n");
 
 var choice = AnsiConsole.Prompt(new SelectionPrompt<string>().PageSize(10)
     .Title("[lime]?[/] What do you [green]want to do[/]?")
-    .AddChoices("Network Overview", "IP Subnetting"));
+    .AddChoices("Network Overview")
+    .AddChoiceGroup("IP Subnetting", new[] {"Uniform hosts", "Varied hosts"}));
 
 switch (choice)
 {
