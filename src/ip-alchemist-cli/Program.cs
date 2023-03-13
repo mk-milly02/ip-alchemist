e9e73c77f56d62b8f7f5b5bedc07d60b7ea2a2c5
Jonas Ababio <jonasagesmilly@outlook.com>
2023-03-13 21:15:23 +0000

    added figlet header and version number

---
+++ src/ip-alchemist-cli/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection;
+using System.Text;
+using Spectre.Console;
+
+Console.Title = "ip-alchemist-cli";
+Console.OutputEncoding = Encoding.UTF8;
+
+AnsiConsole.Write(new FigletText("ip-alchemist-cli").Color(Color.DarkSlateGray2));
+
+var version = Assembly.GetExecutingAssembly().GetName().Version;
+AnsiConsole.MarkupLine("[italic]version {0}[/]".PadLeft(110), version);
+
+Console.ReadLine();
