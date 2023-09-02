a711e11db4cfa27199c24740be7c58beebe5b2b6
Jonas Ababio <jonasagesmilly@outlook.com>
2023-09-02 08:27:57 +0000

    added position and resize to application window

--- src/ip-alchemist.gui/App.xaml.cs
+++ src/ip-alchemist.gui/App.xaml.cs
@@ -8,4 +8,25 @@ public partial class App : Application
 
 		MainPage = new AppShell();
 	}
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        Window window = base.CreateWindow(activationState);
+
+#if WINDOWS
+        window.MaximumWidth = 900;
+        window.MinimumWidth = 900;
+        window.MaximumHeight = 650;
+        window.MinimumHeight = 650;
+
+        //get display size
+        DisplayInfo info = DeviceDisplay.Current.MainDisplayInfo;
+
+        //center the window
+        window.X = (info.Width / info.Density - window.Width) / 2;
+        window.Y = (info.Height / info.Density - window.Height) / 2;
+#endif
+
+        return window;
+    }
 }
